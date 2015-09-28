47bc3619f8e91cc48842888151dc807c93bdbe84
西出 彩花 <nico.mskm.sayaka@gmail.com>
2015-09-28 13:42:08 +0900

    中のファイルを削除

--- nicolara/app/Http/Controllers/WelcomeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Http\Controllers;
-use App\Http\Controllers\Controller;
-
-class WelcomeController extends Controller {
-    public function contact(){
-        return view("contact");
-    }
-}
--- nicolara/resources/views/contact.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- app/resources/view/contact.blade.php -->
-@extends ('layout')
-
-@section('content')
-    <h1> contact me </h1>
-@endsection
-
