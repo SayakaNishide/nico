6cfbe3d4ea587110c9d9311fb1eb61f5c5723bf4
西出 彩花 <nico.mskm.sayaka@gmail.com>
2015-10-22 13:48:13 +0900

    場所検索の表示方法が難しい(検索結果がみつかりませんでした、をどう表示するか)

--- public/printplaces.php
+++ public/printplaces.php
@@ -6,7 +6,6 @@
 </head>
 <body>
 <p><?php
-        
         $link = pg_connect("host=localhost dbname=testdb user=SayakaNishide password=password");
         if (!$link) {
         die('DB接続失敗'.pg_last_error());
@@ -16,41 +15,59 @@
         die('クエリーが失敗しました。'.pg_last_error());
         }
 
-
+        print("検索結果:<br />");
 
         for ($i = 0 ; $i < pg_num_rows($result) ; $i++){
             $rows = pg_fetch_array($result, NULL, PGSQL_ASSOC);
-            if($_GET["ward"] == null){
-                //print($_GET["ward"]);
-                //print("を含む施設：<br />");
+            if(!$_GET["ward"]){
                 if($_GET["name"] == $rows['place_name']){  
-                print("施設番号：");          
-                print($rows['place_no']);
-                print("<br />施設名：");   
-                print($rows['place_name']);
-                print('<br />');}
-            }else if($_GET["name"] == null){
-                print($_GET["ward"]);
-                print("にある施設：<br />"); 
+                    print("<br />施設名：");   
+                    print($rows['place_name']);
+                    print('<br />');
+                    print("空いているか(空欄:空き　1:予約済み)：");
+                    print($rows['place_able']);
+                    print('<br />');
+                }else if($_GET["ward"] != $rows['place_ward']){
+                    print("条件に合う施設はありませんe");
+                    break;
+                }else if(!$_GET["name"]){
+                    print("条件に合う施設はありませんa");
+                    break;
+                }
+            }
+
+            if(!$_GET["name"]){
                 if($_GET["ward"] == $rows['place_ward']){ 
-                print("施設番号：");      
-                print($rows['place_no']);
-                print("<br />施設名：");  
-                print($rows['place_name']);
-                print('<br />');}
-            }else if($_GET["ward"] == $rows['place_ward'] && $_GET["name"] == $rows['place_name']){ 
-                //print("条件に合う施設<br />");
-                //print("施設番号:");           
-                print($rows['place_no']);
-                print("<br />施設名："); 
+                    print("<br />施設名：");  
+                    print($rows['place_name']);
+                    print('<br />');
+                    print("空いているか(空欄:空き　1:予約済み)：");
+                    print($rows['place_able']);
+                    print('<br />');
+                }else if($_GET["ward"] != $rows['place_ward']){
+                    print("条件に合う施設はありませんd");
+                    break;
+                }else if(!$_GET["ward"]){
+                    print("条件に合う施設はありませんb");
+                    break;
+                }
+            }
+
+            if($_GET["ward"] == $rows['place_ward'] && $_GET["name"] == $rows['place_name']){       
+                print("施設名："); 
                 print($rows['place_name']);
                 print('<br />');
-            }else{
-                print("条件に合う施設はありません");
+                print("空いているか(空欄:空き　1:予約済み)：");
+                print($rows['place_able']);
+                print('<br />');
+            }
+
+            if ($_GET["ward"] != $rows['place_ward'] || $_GET["name"] != $rows['place_name']){
+                print("条件に合う施設はありませんc");
                 break;
             }
         }
-   
+
         $close_flag = pg_close($link);
         if (!$close_flag){
             print('切断失敗<br>');
